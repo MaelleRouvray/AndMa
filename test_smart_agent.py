f52800be71476c0e69a37ba8799f7f29dbcb3aaa
Maelle Rouvray <maelle.rouvray@gmail.com>
2025-12-08 00:53:58 +0100

    edit

--- test_smart_agent.py
+++ test_smart_agent.py
@@ -1,73 +1,153 @@
-# test avec un agent random VS un agent smart
-import random_agent as rd_ag
 import smart_agent as sm_ag
-
+import numpy as np
+import pytest
 from pettingzoo.classic import connect_four_v3
 
-def multi_play(num_games):
+@pytest.fixture
+def agent():
+    """Fixture qui fournit une instance de SmartAgent à chaque test"""
+    env=connect_four_v3.env(render_mode="human")
+    return sm_ag.SmartAgent(env)
+
+def test_get_valid_actions(agent):
+    """Test de la fonction _get_valid_actions"""
+    assert agent._get_valid_actions([1, 1, 1, 1, 1, 1, 1]) == [0, 1, 2, 3, 4, 5, 6]
+    assert agent._get_valid_actions([0, 1, 0, 1, 0, 1, 0]) == [1, 3, 5]
+
+
+def test_find_winning_move(agent):
+    """Test de la fonction _find_winning_move"""
+    observation = {"observation": np.zeros((6, 7, 2))}
+    #pions du joueur 0
+    observation["observation"][0, 2,0]=1 
+    observation["observation"][0, 4,0]=1
+    observation["observation"][1, 2,0]=1
+    
+    valid_actions = [0, 1, 2, 3, 4, 5, 6]
+    channel=0
+    #cas pas de victoire
+    assert agent._find_winning_move(observation, valid_actions,channel)==None
+    #cas victoire:
+    observation2 =  {"observation": np.zeros((6, 7, 2))}
+    #pions du joueur 0
+    observation2["observation"][5, 2, 0] = 1
+    observation2["observation"][4, 2, 0] = 1
+    observation2["observation"][3, 2, 0] = 1
+    """État du plateau :
+            . . . . . . .
+            . . . . . . .
+            . . . . . . .
+            . . X . . . .
+            . . X . . . .
+            . . X . . . . 
     """
-        Renvoie les statistiques sur les parties jouées
+    valid_actions2 = [0, 1, 2, 3, 4, 5, 6]
+    channel2=0
+    assert agent._find_winning_move(observation2, valid_actions2, channel2)==2
 
-    Parameters:
-        num_games : nombre de parties à jouer
+def test_get_next_row(agent):
+    """Test de la fonction _get_next_row"""
+    board = np.zeros((6, 7, 2))
+    assert agent._get_next_row(board, 3) == 5
+    board[5, 3, 0] = 1
+    assert agent._get_next_row(board, 3) == 4
 
-    Returns:
-        vic_player0 : nombre de victoires du joueur0
-        vic_player1 : nombre de victoires du joueur1
-        list_coups : liste contenant le nombre de coups joués pour chaque partie
-        egalite : nombre de matchs nuls
-        
+def test_check_win_from_position(agent):
+    """Test de la fonction _check_win_from_position"""
+    #cas pas de victoire
+    board = np.zeros((6, 7, 2))
+    board[0,1,0]=1
+    board[0,3,0]=1
+    """État du plateau :
+            . . . . . . .
+            . . . . . . .
+            . . . . . . .
+            . . . . . . .
+            . . . . . . .
+            . X . X . . .
     """
-    vic_player0 = 0
-    vic_player1 = 0
-    egalite = 0
-    list_coups = []
+    row=0
+    col=4
+    channel=0
+    assert agent._check_win_from_position(board,row , col, channel)==False
 
-    for i in range(num_games):
-        nb_coups = 0
+    #cas 2: victoire horizontale
+    board2 = np.zeros((6, 7, 2))
+    board2[0,1,0]=1
+    board2[0,2,0]=1
+    board2[0,3,0]=1
 
-        env = connect_four_v3.env(render_mode="human") # ou render_mode="rdb_array" ou bien None
-        env.reset(seed=42)
+    """État du plateau :
+            . . . . . . .
+            . . . . . . .
+            . . . . . . .
+            . . . . . . .
+            . . . . . . .
+            . X X X . . .
+    """
+    assert agent._check_win_from_position(board2,row , col, channel)==True
 
-        player0 = rd_ag.RandomAgent(env,"player_0")
-        player1 = sm_ag.SmartAgent(env,"player_1")
+    #cas victoire verticale
+    board3 = np.zeros((6, 7, 2))
+    board3[0,1,0]=1
+    board3[1,1,0]=1
+    board3[2,1,0]=1
 
-        agents = {"player_0": player0,"player_1": player1}
+    """État du plateau :
+            . . . . . . .
+            . . . . . . .
+            . . . . . . .
+            . X . . . . .
+            . X . . . . .
+            . X . . . . .
+    """
+    row3=3
+    col3=1
+    assert agent._check_win_from_position(board3 ,row3 , col3, channel)==True
 
-        for agent in env.agent_iter():
-            observation, reward, termination, truncation, info = env.last()
+    #cas victoire diagonale /
+    board4 = np.zeros((6, 7, 2))
+    board4[0,0,0]=1
+    board4[0,1,1]=1
+    board4[0,2,0]=1
+    board4[0,3,1]=1
+    board4[1,1,0]=1
+    board4[1,2,1]=1
+    board4[1,3,1]=1
+    board4[2,2,0]=1
+    board4[2,3,1]=1
 
-            if termination or truncation:
-                action = None
-                if reward == 1:
-                    print(f"{agent} wins!")
-                    if agent == "player_0" : 
-                        vic_player0 +=1
-                    else :
-                        vic_player1 +=1
-
-                    
-                elif reward == 0:
-                    print("It's a draw!")
-                    egalite += 1
-            else:
-                # Take a random valid action
-                action_mask = observation["action_mask"]
-                if agent == "player_0":
-                    action = agents[agent].choose_action_manual(observation, reward, termination, truncation, info, action_mask)
-                if agent == "player_1":
-                    action = agents[agent].choose_action(observation, reward, termination, truncation, info, action_mask)
-                print(f"{agent} plays column {action}")
-                nb_coups += 1
-
-            env.step(action)
-            
-
-        #input("Press Enter to close...")
-        env.close()
-        # le test s'effectue bien sans problèmes , une partie se terrmine en peu de coups (environ 10 coups par joueur)
-        list_coups.append(nb_coups)
-    print(f"nb victoires player0 : {vic_player0}, nb victoires player1 : {vic_player1},nb de coups par partie : {list_coups} , nb de matchs nuls : {egalite}")
-    return vic_player0, vic_player1, list_coups, egalite 
-
-print(multi_play(1))
+    """État du plateau :
+            . . . . . . .
+            . . . . . . .
+            . . . . . . .
+            . . X 0 . . .
+            . X 0 0 . . .
+            X 0 X 0 . . .
+    """
+    row4=3
+    col4=3
+    assert agent._check_win_from_position(board4 ,row4 , col4, channel)==True
+    
+    #cas victoire diagonale \
+    board5 = np.zeros((6, 7, 2))
+    board5[0,0,1]=1
+    board5[0,1,1]=1
+    board5[0,2,1]=1
+    board5[0,3,0]=1
+    board5[1,0,1]=1
+    board5[1,1,1]=1
+    board5[1,2,0]=1
+    board5[2,0,0]=1
+    board5[2,1,0]=1
+    """État du plateau :
+            . . . . . . .
+            . . . . . . .
+            . . . . . . .
+            X X . . . . .
+            0 0 X . . . .
+            0 0 0 X . . .
+    """
+    col5=0
+    row5=3
+    assert agent._check_win_from_position(board5 ,row5 , col5, channel)==True
